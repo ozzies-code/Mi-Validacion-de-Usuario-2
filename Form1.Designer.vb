7b019a29938ae1193e97ec5ee71b76bfb48f5ab4
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-01-04 20:28:28 -0400

    Agregando Archivos al Repositorio
    Agregando archivos al repositorio local

---
+++ Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRegistro = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'btnRegistro
+        '
+        Me.btnRegistro.BackColor = System.Drawing.Color.IndianRed
+        Me.btnRegistro.Font = New System.Drawing.Font("Colonna MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRegistro.Location = New System.Drawing.Point(129, 177)
+        Me.btnRegistro.Name = "btnRegistro"
+        Me.btnRegistro.Size = New System.Drawing.Size(114, 43)
+        Me.btnRegistro.TabIndex = 0
+        Me.btnRegistro.Text = "Registrarse"
+        Me.btnRegistro.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Colonna MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(345, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Escriba aqui tu numero de Seguridad Social"
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.MaskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.MaskedTextBox1.Font = New System.Drawing.Font("Colonna MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaskedTextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(26, 56)
+        Me.MaskedTextBox1.Mask = "000-00-0000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(342, 24)
+        Me.MaskedTextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Colonna MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "PIN"
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.MaskedTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.MaskedTextBox2.Font = New System.Drawing.Font("Colonna MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaskedTextBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(26, 133)
+        Me.MaskedTextBox2.Mask = "00000"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(107, 24)
+        Me.MaskedTextBox2.TabIndex = 4
+        Me.MaskedTextBox2.ValidatingType = GetType(Integer)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(391, 234)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRegistro)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Validacion de Usuario"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRegistro As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnRegistro_Click(sender As Object, e As EventArgs) Handles btnRegistro.Click
+        'Mascaras de Entrada 
+        If MaskedTextBox1.Text = "555-55-1212" _
+        And MaskedTextBox2.Text = "54321" Then
+            MsgBox("¡Bienvenido al sistema!")
+        Else
+            MsgBox("Número irreconocible")
+        End If
+    End Sub
+End Class
